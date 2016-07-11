9952b080bc4e2b09b1921714baf9b186dec12962
Claudio Mignanti <c.mignanti@gmail.com>
2016-07-11 17:56:50 +0200

    first package parse

--- adeunis-rf.js
+++ adeunis-rf.js
@@ -19,15 +19,37 @@ module.exports = function (RED) {
             batt_is_present = status & 0x02;
             rssi_is_present = status & 0x01;
 
+            ret.temp = msg.payload[1]
+            ret.btn1 = btn1_is_present
 
+            //gps data absent
+            if (gps_is_present) {
+                shift = 8
+                ret.gps=true
+                ret.lat = ((msg.payload[2] & 0xF0 ) >> 4)*10 + (msg.payload[2] & 0x0F)
+                ret.lat += (((msg.payload[3] & 0xF0 ) >> 4)*10 + (msg.payload[3] & 0x0F)) / 60
+                ret.lat += ((((msg.payload[4] & 0xF0) >> 4) * 0.1) +
+                             ((msg.payload[4] & 0x0F) * 0.01 ) +
+                             (((msg.payload[5] & 0xF0) >> 4) *0.001)) /60
 
-            msg.payload.length
+                ret.lon = ((msg.payload[6] & 0xF0 ) >> 4)*100 + (msg.payload[6] & 0x0F )*10 + ((msg.payload[7] & 0xF0) >> 4) // degree
+                ret.lon += (((msg.payload[7] & 0x0F )*10 + (msg.payload[8] & 0xF0) >> 4)) /60
+                ret.lon += (((msg.payload[8] & 0x0F) * 0.1) + (((msg.payload[9] & 0xF0) >> 4) * 0.01)) /60
 
+            } else {
+                shift = 0
+                ret.gps=false
+            }
 
+            ret.uplink = msg.payload[2+shift];
+            ret.down = msg.payload[3+shift];
+            ret.batt = msg.payload[4+shift] << 8 + msg.payload[5+shift];
+
+            msg.raw = msg.payload;
             msg.payload = ret;
 
             node.send(msg);
         });
     }
-    RED.nodes.registerType("adeunis-rf",LowerCaseNode);
+    RED.nodes.registerType("adeunis-rf",AdeunisRF);
 }
